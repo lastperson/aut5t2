34c3206aeab7f1aa1709f7f48e3eb4b15c355d0b
Oleksii Matiiasevych <lastperson@gmail.com>
2015-03-25 19:55:10 +0200

    First main.java added.

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*int a = 256 + 3180;
+	    System.out.println("Sum of 256 and 3180: " + a);
+        double e = 180.0/31.0;
+        System.out.println("180/31 is " + e);
+        double f = 10.0;
+        int g = 20;
+        double j =99.0;
+        j = f / g;
+        System.out.println(j);
+        String h = "Our results:\n 10\n  20\n 358";
+        System.out.print(h);
+        String i = "asdasasd "+
+                "asdasdasdasdasd" +
+                "asasdasdasd";
+        String g1 = "abc";
+        String j1 = "ab" + "c";
+        System.out.print(g1 == j1);
+
+        int[] h1 = {10,0,0,21,-5,38};
+
+        int e1 = h1[0];
+        int e2 = h1[1];
+        int e3 = h1[2];
+        int e4 = h1[3];
+        int e5 = h1[4];
+        int e6 = h1[5];
+
+        System.out.println("" + e1 + "," + e2 + e3 + e4 + e5 + e6);
+*/
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        System.out.println(args[3]);
+/*
+        String[] j2 = {"a", "b", "c"};
+        String[] j3 = {"a", "d", "c"};
+
+        // a - a, b - d, c - c - Должно появится в консоли
+    */
+    }
+}
