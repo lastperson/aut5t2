a0258fe33c77cc19401b2d6bcb8c6c004db9cb2f
Oleksii Matiiasevych <lastperson@gmail.com>
2015-03-25 21:14:54 +0200

    Main2 is here!

---
+++ src/com/company/Main2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main2 {
+    public static void main(String[] args) {
+        // System.out.println(args[0]);
+
+        // Если первьій єлемент args равен "Hello" тогда напечатать в консоль
+        // "Class", иначе напечатать в консоль "Good bye".
+
+        // Если первьій єлемент args равен "Hello", а второй "Class"
+        // тогда напечатать в консоль "Success"
+        // иначе напечатать в консоль "Good bye".
+
+    }
+}
